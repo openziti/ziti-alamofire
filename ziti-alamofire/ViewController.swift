fd3b185f2ba061bca34d50aa4be7c3f8ad485ae4
Dave Hart <dphart@rochester.rr.com>
2020-08-16 14:01:44 -0400

    Initial Commit

---
+++ ziti-alamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ziti-alamofire
+//
+//  Created by David Hart on 8/16/20.
+//  Copyright © 2020 NetFoundry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
